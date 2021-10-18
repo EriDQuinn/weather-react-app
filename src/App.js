8439af8ca06db2d117ca81ba2efd2c59496ae682
ErikaDiaz <edaeri@live.com.mx>
2021-10-18 00:24:24 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import logo from "./logo.svg";
+import Weather from "./Weather";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <div className="container rounded border m-5 p-2">
+        <h1 className=" title text-center m-2">Weather app</h1>
+        <Weather />
+
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://www.linkedin.com/in/erika-diaz-0b9857195/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Erika Dìaz{" "}
+          </a>{" "}
+          and it is{" "}
+          <a
+            href="https://github.com/EriDQuinn/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Open sourced on GitHub
+          </a>
+        </footer>
+      </div>
+    </div>
+  );
 }
 
 export default App;
