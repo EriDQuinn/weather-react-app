dd470bb08a5e6707ad96048214453104142da57b
ErikaDiaz <edaeri@live.com.mx>
2021-10-18 01:47:26 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,31 +3,30 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <div className="container rounded border m-5 p-2">
+    <div className="App p-5">
+      <div className="container main-cont rounded border m-5 p-2">
         <h1 className=" title text-center m-2">Weather app</h1>
         <Weather />
-
-        <footer>
-          This project was created by{" "}
-          <a
-            href="https://www.linkedin.com/in/erika-diaz-0b9857195/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Erika Dìaz{" "}
-          </a>{" "}
-          and it is{" "}
-          <a
-            href="https://github.com/EriDQuinn/weather-react-app"
-            target="_blank"
-            rel="noreferrer"
-          >
-            {" "}
-            Open sourced on GitHub
-          </a>
-        </footer>
       </div>
+      <footer className=" m-5">
+        This project was created by{" "}
+        <a
+          href="https://www.linkedin.com/in/erika-diaz-0b9857195/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Erika Dìaz{" "}
+        </a>{" "}
+        and it is{" "}
+        <a
+          href="https://github.com/EriDQuinn/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
