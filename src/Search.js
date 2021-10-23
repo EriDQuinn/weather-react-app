1c0bb4e002736922cb55d7886f9863e563c6d25f
ErikaDiaz <edaeri@live.com.mx>
2021-10-23 00:23:49 -0500

    Adding the info from the search

--- src/Search.js
+++ src/Search.js
@@ -1,20 +1,21 @@
 import React from "react";
+import OurDate from "./OurDate";
 
 export default function Search(props) {
   return (
     <div className="Search">
-      <h4 className="text-center">{data.city}</h4>
+      <h4 className="text-center">{props.data.city}</h4>
       <h4 className="icon">
-        <img src={data.iconUrl} alt={data.description}></img>
+        <img src={props.data.iconUrl} alt={props.data.description}></img>
       </h4>
-      <h2 className="temperature"> {Math.round(data.temp)} °C</h2>
+      <h2 className="temperature"> {Math.round(props.data.temp)} °C</h2>
       <div>
-        <OurDate fecha={data.d} />
+        <OurDate fecha={props.data.d} />
       </div>
       <hr></hr>
-      <p>Description: {data.description} </p>
-      <p>Humidity: {data.humidity} %</p>
-      <p>Wind: {data.wind} km/h</p>
+      <p>Description: {props.data.description} </p>
+      <p>Humidity: {props.data.humidity} %</p>
+      <p>Wind: {props.data.wind} km/h</p>
     </div>
   );
 }
