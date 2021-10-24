577329edf48dc11f076c0e1b6870bd49d6dbac49
ErikaDiaz <edaeri@live.com.mx>
2021-10-24 02:02:15 -0500

    Update Search.js

--- src/Search.js
+++ src/Search.js
@@ -1,12 +1,13 @@
 import React from "react";
 import OurDate from "./OurDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function Search(props) {
   return (
     <div className="Search">
       <h4 className="text-center">{props.data.city}</h4>
       <h4 className="icon">
-        <img src={props.data.iconUrl} alt={props.data.description}></img>
+        <WeatherIcon code={props.data.icon} alt={props.data.description} />
       </h4>
       <h2 className="temperature"> {Math.round(props.data.temp)} °C</h2>
       <div>
