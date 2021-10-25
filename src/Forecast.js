8d9996d0670257012269a790a9f511ad4f43ff2d
ErikaDiaz <edaeri@live.com.mx>
2021-10-25 00:28:53 -0500

    Update Forecast.js
    inserting icons from component

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,40 +1,24 @@
 import React from "react";
-import ReactAnimatedWeather from "react-animated-weather";
 import "./forecast.css";
+import WeatherIcon from "./WeatherIcon";
 
-const defaults = {
-  icon: "CLEAR_DAY",
-  color: "goldenrod",
-  size: 22,
-  animate: true,
-};
 export default function Forecast() {
   return (
     <div className="Forecast container">
       <div className="row">
         <div className="col">
           <div className="card p-3 bg-transparent border border-white">
-            <p>Monday</p>
-
-            <ReactAnimatedWeather
-              icon={defaults.icon}
-              color={defaults.color}
-              size={defaults.size}
-              animate={defaults.animate}
-            />
-
-            <p>10ºC</p>
+            <p>Monday</p> <WeatherIcon code="50d" size={36} />
+            <div>
+              <span className="TempMin">10ºC</span>
+              <span className="TempMax">20ºC</span>
+            </div>
           </div>
         </div>
         <div className="col">
           <div className="card p-3 bg-transparent col border border-white">
             <p>Tuesday</p>
-            <ReactAnimatedWeather
-              icon="CLOUDY"
-              color="gray"
-              size={defaults.size}
-              animate={defaults.animate}
-            />
+            <WeatherIcon code="50d" />
             <p>10ºC</p>
           </div>
         </div>
@@ -43,12 +27,7 @@ export default function Forecast() {
         <div className="col">
           <div className="card p-3 bg-transparent border border-white">
             <p>Wednesday</p>
-            <ReactAnimatedWeather
-              icon="RAIN"
-              color="blue"
-              size={defaults.size}
-              animate={defaults.animate}
-            />
+            <WeatherIcon code="50d" />
             <p>10ºC</p>
           </div>
         </div>
@@ -56,12 +35,7 @@ export default function Forecast() {
           <div className="card p-3 bg-transparent border border-white">
             <div className="card-header">Thursday</div>
 
-            <ReactAnimatedWeather
-              icon={defaults.icon}
-              color={defaults.color}
-              size={defaults.size}
-              animate={defaults.animate}
-            />
+            <WeatherIcon code="50d" />
 
             <p>10ºC</p>
           </div>
@@ -71,24 +45,14 @@ export default function Forecast() {
         <div className="col">
           <div className="card p-3 bg-transparent border border-white">
             <p>Friday</p>
-            <ReactAnimatedWeather
-              icon="WIND"
-              color="white"
-              size={defaults.size}
-              animate={defaults.animate}
-            />
+            <WeatherIcon code="50d" />
             <p>10ºC</p>
           </div>
         </div>
         <div className="col">
           <div className="card p-3 bg-transparent border border-white">
             <p>Saturday</p>
-            <ReactAnimatedWeather
-              icon={defaults.icon}
-              color={defaults.color}
-              size={defaults.size}
-              animate={defaults.animate}
-            />
+            <WeatherIcon code="50d" />
             <p>10ºC</p>
           </div>
         </div>{" "}
