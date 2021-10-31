44da72909d27776a6bc64ef025fe77ca37ada859
ErikaDiaz <edaeri@live.com.mx>
2021-10-31 17:53:18 -0600

    Create ForecastDay.js
    Creating this component to render the day at forecast

---
+++ src/ForecastDay.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function ForecastDay(props) {
+  function day() {
+    let date = new Date(props.data.dt * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+    return days[day];
+  }
+  function maxTemp() {
+    let temp = Math.round(props.data.temp.max);
+    return `${temp}º`;
+  }
+  function minTemp() {
+    let temp = Math.round(props.data.temp.min);
+    return `${temp}º`;
+  }
+  return (
+    <div className="card p-3 bg-transparent border border-white">
+      <div className="card-header">{day()}</div>{" "}
+      <WeatherIcon code={props.data.weather["0"].icon} size={36} />
+      <div>
+        <span className="TempMin">{minTemp()}</span>
+        <span className="TempMax">{maxTemp()}</span>
+      </div>
+    </div>
+  );
+}
